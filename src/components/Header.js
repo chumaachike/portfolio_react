50826fb708419ae27a30babe3b640a3a4186d70c
chumaachike <edwardachike@gmail.com>
2023-12-29 22:29:55 +0000

    finising up react portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,43 @@
+import { useState } from 'react';
 import imgMenu from '../images/menu.png';
+import Popup from './Popup';
 
 function Header() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClick = () => {
+    setShowPopup(true);
+  };
+
   return (
     <header className="flex">
-      {/* Mobile version */}
-      <button type="button" className="logo-container flex" href="#">
-        <span className="logo">Welcome</span>
-      </button>
-      <nav className="menu-button">
-        <button type="button" className="menu-anchor flex" href="#">
-          <img
-            src={imgMenu}
-            width="20"
-            height="20"
-            alt="Menu"
-            title="Menu"
-          />
-        </button>
-      </nav>
+      {showPopup ? (
+        <Popup togglePopup={setShowPopup} />
+      ) : (
+        <>
+          {/* Mobile version */}
+          <button type="button" className="logo-container flex" href="#">
+            <span className="logo">Welcome</span>
+          </button>
+          <nav className="menu-button">
+            <button
+              type="button"
+              className="menu-anchor flex"
+              href="#"
+              onClick={handleClick}
+            >
+              <img
+                src={imgMenu}
+                width="20"
+                height="20"
+                alt="Menu"
+                title="Menu"
+              />
+            </button>
+          </nav>
+        </>
+      )}
+
       {/* --Desktop version */}
       <nav className="menu-bar">
         <a className="menu-opt-cont flex" href="#projects">
@@ -29,8 +49,15 @@ function Header() {
         <a className="menu-opt-cont flex" href="#form">
           <span className="menu-opt">Contact</span>
         </a>
-        <a className="menu-opt-cont flex" href="https://www.youtube.com/channel/UCKmOGDm2gQ-XydtP0Xrt5zQ" target="_blank" rel="noreferrer">
-          <button type="button" className="menu-opt" id="blog-button">Youtube</button>
+        <a
+          className="menu-opt-cont flex"
+          href="https://www.youtube.com/channel/UCKmOGDm2gQ-XydtP0Xrt5zQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button" className="menu-opt" id="blog-button">
+            Youtube
+          </button>
         </a>
       </nav>
     </header>
---
+++ src/components/Popup.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import close from '../images/close.png';
+
+function Popup({ togglePopup }) {
+  const handleClick = () => {
+    togglePopup(false);
+  };
+  return (
+    <nav className="interactive-menu">
+      <img src={close} alt="Close menu" title="Close menu" className="close-menu" onClick={handleClick} />
+      <a href="#works" className="menu-option" onClick={handleClick}>Portfolio</a>
+      <a href="#about" className="menu-option" onClick={handleClick}>About</a>
+      <a href="#form" className="menu-option" onClick={handleClick}>Contact</a>
+    </nav>
+  );
+}
+Popup.propTypes = {
+    togglePopup: PropTypes.func.isRequired,
+};
+
+export default Popup;
